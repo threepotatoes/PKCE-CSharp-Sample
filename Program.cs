e55667d6151695010820347d62aef71451066185
Summer <victoriasqq@gmail.com>
2019-08-28 11:19:37 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sample
+{
+    public class PkceApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating a pkce app.");
+        }
+    }
+}
